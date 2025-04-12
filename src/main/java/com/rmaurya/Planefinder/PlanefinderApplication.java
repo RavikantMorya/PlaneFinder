932f97a4a3a9510556a98dae8e402da6b622bcd7
RavikantMorya <mauryaravikant555@gmail.com>
2025-04-12 18:39:21 +0530

    Main Java File to Run Application

---
+++ src/main/java/com/rmaurya/Planefinder/PlanefinderApplication.java
@@ -0,0 +1,13 @@
+package com.rmaurya.Planefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanefinderApplication.class, args);
+	}
+
+}
